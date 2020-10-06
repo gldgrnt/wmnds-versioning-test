9f6512a8cd10548ac918bf7b1021494cd94c8646
Gilad Grant <ggrant966@gmail.com>
2020-10-06 11:32:56 +0100

    refactor: add gulp-tasks directory

---
+++ gulp-tasks/build-styles.js
@@ -0,0 +1,11 @@
+const { src, dest } = require("gulp");
+const plugins = require("gulp-load-plugins")();
+const paths = require("./paths");
+
+module.exports = () => {
+  return src(`${paths.srcDir}sass/*.scss`)
+    .pipe(plugins.sass().on("error", plugins.sass.logError))
+    .pipe(plugins.autoprefixer()) // Prefix css with older browser support
+    .pipe(plugins.cleanCss({ level: 2 })) // Minify css
+    .pipe(dest(paths.outputDir));
+};
---
+++ gulp-tasks/paths.js
@@ -0,0 +1,4 @@
+module.exports = {
+  srcDir: "./src/",
+  outputDir: "./build",
+};
